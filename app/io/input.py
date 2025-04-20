9afae228c0f0963bc3503f4fccb5ba1ee3ceb496
AnastasiiaPravylo <praviloanastasiia@gmail.com>
2025-04-20 18:51:40 +0300

    add functions to files i/o

---
+++ app/io/input.py
@@ -0,0 +1,27 @@
+def read_from_console():
+    """
+    Зчитує текст з консолі
+    Returns:
+        str: Введений текст з консолі
+    """
+    pass
+
+def read_from_file_builtin():
+    """
+    Зчитує вміст файлу за допомогою вбудованих засобів
+    Args:
+        file_path (str): шлях до файлу
+    Returns:
+        str: вміст що знаходться у файлі
+    """
+    pass
+
+def read_from_file_pandas():
+    """
+    Зчитує файл за допомогою бібліотеки pandas
+    Args:
+        file_path (str): шлях до файлу
+    Returns:
+        pd.DataFrame: вміст файлу у вигляді таблиці
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,16 @@
+def print_output_to_console():
+    """
+    Виводить текст у консоль
+    Args:
+        data (str): текст що має виводитись
+    """
+    pass
+
+def write_to_file_builtin():
+    """
+    Записує текст у файл за допомогою вбудованих засобів Python
+    Args:
+        file_path (str): шлях до файлу
+        data (str): текст що має записуватись
+    """
+    pass
